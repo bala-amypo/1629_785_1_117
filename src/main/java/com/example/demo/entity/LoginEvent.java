479c039fa543849c0d47b0a1d0550a341d0ce283
bala-amypo <bala@amypo.in>
2025-12-18 23:48:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoginEvent.java
+++ src/main/java/com/example/demo/entity/LoginEvent.java
@@ -1,1 +1,73 @@
 package com.example.demo.entity
+// package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class LoginEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String ipAddress;
+    private String loginStatus;
+    private LocalDateTime loginTime;
+
+    public LoginEvent() {
+    }
+
+    public LoginEvent(Long id, Long userId, String ipAddress, String loginStatus, LocalDateTime loginTime) {
+        this.id = id;
+        this.userId = userId;
+        this.ipAddress = ipAddress;
+        this.loginStatus = loginStatus;
+        this.loginTime = loginTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    public LocalDateTime getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(LocalDateTime loginTime) {
+        this.loginTime = loginTime;
+    }
+}
