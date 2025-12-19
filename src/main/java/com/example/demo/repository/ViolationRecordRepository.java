549349d3bb0da3a5ec440176e1e4900b281bb897
bala-amypo <bala@amypo.in>
2025-12-19 01:23:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/ViolationRecordRepository.java
+++ src/main/java/com/example/demo/repository/ViolationRecordRepository.java
@@ -1,2 +1,6 @@
 package com.example.demo.repository
 
+public interface ViolationRecordRepository extends JpaReository<ViolationRrcord,Long>{
+    List<ViolationRecord>findByUserId(Long,userId);
+    List<ViolationRecord>findByResolvedFalse()
+}
