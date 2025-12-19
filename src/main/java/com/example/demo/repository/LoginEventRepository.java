b5fb9da11de7c189c1b4ee33bff5638ef6d5e191
bala-amypo <bala@amypo.in>
2025-12-19 00:50:01 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoginEventRepository.java
+++ src/main/java/com/example/demo/repository/LoginEventRepository.java
@@ -2,8 +2,11 @@ package com.example.demo.repository
 
 import com.example.demo.entity.LoginEvent;
 import org.sprinframework.data.Jpa.repository.JpaRepository;
+import java.util.List;
 
 
 public interface LoginEventRepository extends JpaRepository<LoginEvent,Long>{
-
+    
+   List<LoginEvent>findByUserId(Londg userId);
+   List<LoginEvent>findByUserIdAndLoginStatus(Long userId,String loginStatus);
 }
