f5142831951dae7e27ecbbed270c7261125029f8
bala-amypo <bala@amypo.in>
2025-12-21 13:33:34 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -14,7 +14,7 @@ import com.example.demo.exception.BadRequestException;
 @Service
 public class UserAccountServiceImpl implements UserAccountService {
 
-    private final UserAccountRepository userRepo;
+    private UserAccountRepository userRepo;
 
     public UserAccountServiceImpl(UserAccountRepository userRepo) {
         this.userRepo = userRepo;
