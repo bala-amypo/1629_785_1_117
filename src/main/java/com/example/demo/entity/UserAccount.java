3b6df9166eb336011e0c7c8869bbb70e27b0f552
bala-amypo <bala@amypo.in>
2025-12-21 12:26:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -0,0 +1,39 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_accounts")
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String employeeId;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String role;
+    private String status;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        if (this.status == null) {
+            this.status = "ACTIVE";
+        }
+    }
+}
--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -1,12 +1,10 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import com.example.demo.entity.UserAccount;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import java.util.Optional;
+import com.example.demo.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-// public interface UserAccountRepository extends JpaRepository<UserAccount,long>{
-
-// Optional<UserAccount>findByUsername(String username);  //unique username
-// Optional<UserAccount>findByEmail(String Email);        //unique email
-
-// }
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByUsername(String username);
+    Optional<UserAccount> findByEmail(String email);
+}
