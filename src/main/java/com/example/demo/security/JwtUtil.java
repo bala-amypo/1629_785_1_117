a531ae5b01565640e35451d1d36fb8b6312422f9
bala-amypo <bala@amypo.in>
2025-12-22 01:10:06 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,34 +1,34 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
-public class JwtUtil {
+// public class JwtUtil {
 
-    private String secret;
-    private long validityInMs;
-    private boolean isTestMode;
+//     private String secret;
+//     private long validityInMs;
+//     private boolean isTestMode;
 
-    public JwtUtil(String secret, long validityInMs, boolean isTestMode) {
-        this.secret = secret;
-        this.validityInMs = validityInMs;
-        this.isTestMode = isTestMode;
-    }
+//     public JwtUtil(String secret, long validityInMs, boolean isTestMode) {
+//         this.secret = secret;
+//         this.validityInMs = validityInMs;
+//         this.isTestMode = isTestMode;
+//     }
 
-    public String generateToken(String username, Long userId, String email, String role) {
-        return null;
-    }
+//     public String generateToken(String username, Long userId, String email, String role) {
+//         return null;
+//     }
 
-    public boolean validateToken(String token) {
-        return false;
-    }
+//     public boolean validateToken(String token) {
+//         return false;
+//     }
 
-    public String getEmail(String token) {
-        return null;
-    }
+//     public String getEmail(String token) {
+//         return null;
+//     }
 
-    public String getRole(String token) {
-        return null;
-    }
+//     public String getRole(String token) {
+//         return null;
+//     }
 
-    public Long getUserId(String token) {
-        return null;
-    }
-}
+//     public Long getUserId(String token) {
+//         return null;
+//     }
+// }
