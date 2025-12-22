405a100d796157a49cdcb0fdb57023b3fdbcbfd3
bala-amypo <bala@amypo.in>
2025-12-22 01:45:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceProfileController.java
+++ src/main/java/com/example/demo/controller/DeviceProfileController.java
@@ -22,8 +22,7 @@ public class DeviceProfileController {
     }
 
     @PutMapping("/{id}/trust")
-    public DeviceProfile trust(@PathVariable Long id,
-                               @RequestParam boolean trust) {
+    public DeviceProfile trust(@PathVariable Long id,@RequestParam boolean trust) {
         return service.updateTrustStatus(id, trust);
     }
 
--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -32,3 +32,4 @@ public class JwtUtil {
         return null;
     }
 }
+
