97a80a7165a7f7196c6b4063a697ef48dc468fe8
bala-amypo <bala@amypo.in>
2025-12-26 15:27:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse {
+
+    private String token;
+    private Long userId;
+    private String email;
+    private String role;
+}
---
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    private String employeeId;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+}
