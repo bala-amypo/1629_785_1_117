777bdf3b1181752e0fd24f0152769c6b4370295c
bala-amypo <bala@amypo.in>
2025-12-27 04:46:50 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,13 +1,13 @@
-package com.example.demo.servlet;
+// package com.example.demo.servlet;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.WebServlet;
-import java.io.IOException;
+// import jakarta.servlet.http.*;
+// import jakarta.servlet.annotation.WebServlet;
+// import java.io.IOException;
 
-@WebServlet(urlPatterns = "/status")
-public class SimpleStatusServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res)
-            throws IOException {
-        res.getWriter().write("Servlet Running");
-    }
-}
+// @WebServlet(urlPatterns = "/status")
+// public class SimpleStatusServlet extends HttpServlet {
+//     protected void doGet(HttpServletRequest req, HttpServletResponse res)
+//             throws IOException {
+//         res.getWriter().write("Servlet Running");
+//     }
+// }
