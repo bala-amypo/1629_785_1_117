4af2d8f017191aff52bf89c4c3c752f57a4e58b9
bala-amypo <bala@amypo.in>
2025-12-27 04:02:56 +0000

    User commit

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,23 +1,23 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import com.example.demo.security.JwtUtil;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import com.example.demo.security.JwtUtil;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class AppConfig {
+// @Configuration
+// public class AppConfig {
 
-    // JwtUtil bean
-    @Bean
-    public JwtUtil jwtUtil() {
-        return new JwtUtil("TestSecretKeyForJWT1234567890", 3600000L, true);
-    }
+//     // JwtUtil bean
+//     @Bean
+//     public JwtUtil jwtUtil() {
+//         return new JwtUtil("TestSecretKeyForJWT1234567890", 3600000L, true);
+//     }
 
-    // PasswordEncoder bean
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//     // PasswordEncoder bean
+//     @Bean
+//     public PasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
+// }
