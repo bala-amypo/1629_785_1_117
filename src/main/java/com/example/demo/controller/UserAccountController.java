0daead619a48ab47319914518ebf10a78e124a33
bala-amypo <bala@amypo.in>
2025-12-28 12:56:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import com.example.demo.entity.UserAccount;
 import com.example.demo.service.UserAccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
@@ -12,28 +11,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-@SecurityRequirement(name = "BearerAuth")   // 🔐 lock visible in Swagger
+@SecurityRequirement(name = "BearerAuth")
 public class UserAccountController {
 
     private final UserAccountService userService;
 
     @PostMapping
-    public ResponseEntity<UserAccount> create(@RequestBody UserAccount user){
-        return ResponseEntity.ok(userService.createUser(user));
+    public UserAccount create(@RequestBody UserAccount user){
+        return userService.createUser(user);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserAccount> getById(@PathVariable Long id){
-        return ResponseEntity.ok(userService.getUserById(id));
+    public UserAccount getById(@PathVariable Long id){
+        return userService.getUserById(id);
     }
 
     @GetMapping
-    public ResponseEntity<List<UserAccount>> getAll(){
-        return ResponseEntity.ok(userService.getAllUsers());
+    public List<UserAccount> getAll(){
+        return userService.getAllUsers();
     }
 
     @PutMapping("/{id}/status/{status}")
-    public ResponseEntity<UserAccount> updateStatus(@PathVariable Long id, @PathVariable String status){
-        return ResponseEntity.ok(userService.updateUserStatus(id,status));
+    public UserAccount updateStatus(@PathVariable Long id,@PathVariable String status){
+        return userService.updateUserStatus(id,status);
     }
 }
