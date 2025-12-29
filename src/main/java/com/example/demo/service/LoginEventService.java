6d20e36d1e0191f2ad50c90cb85d4e13e6bd7673
bala-amypo <bala@amypo.in>
2025-12-29 01:53:53 +0000

    User commit

--- src/main/java/com/example/demo/service/LoginEventService.java
+++ src/main/java/com/example/demo/service/LoginEventService.java
@@ -25,14 +25,3 @@ public interface LoginEventService {
 
 
 
-
-// package com.example.demo.service;
-
-// import com.example.demo.entity.LoginEvent;
-// import java.util.List;
-
-// public interface LoginEventService {
-//     LoginEvent recordLogin(LoginEvent event);      // FINAL
-//     List<LoginEvent> getEventsByUser(Long userId);
-//     List<LoginEvent> getSuspiciousLogins(Long userId);
-// }
