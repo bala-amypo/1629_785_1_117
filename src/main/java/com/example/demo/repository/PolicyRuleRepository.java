a72a035985aeb8bdf43e033465a7a1ec55b11ff4
bala-amypo <bala@amypo.in>
2025-12-29 05:12:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRuleRepository.java
@@ -1,24 +1,24 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.entity.PolicyRule;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import java.util.List;
-
-// public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
-//     List<PolicyRule> findByActiveTrue();   // REQUIRED BY TESTCASE #17 & #19 & #20
-// }
-
-
-
-
-
 package com.example.demo.repository;
 
-import com.example.demo.entity.*;
-import java.util.*;
+import com.example.demo.entity.PolicyRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface PolicyRuleRepository {
-    PolicyRule save(PolicyRule r);
-    List<PolicyRule> findByActiveTrue();
-    List<PolicyRule> findAll();
+public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
+    List<PolicyRule> findByActiveTrue();   // REQUIRED BY TESTCASE #17 & #19 & #20
 }
+
+
+
+
+
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.*;
+// import java.util.*;
+
+// public interface PolicyRuleRepository {
+//     PolicyRule save(PolicyRule r);
+//     List<PolicyRule> findByActiveTrue();
+//     List<PolicyRule> findAll();
+// }
