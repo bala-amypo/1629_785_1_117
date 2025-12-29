5fa34320c32b3fa84051049d2d669d52b2edb1b5
bala-amypo <bala@amypo.in>
2025-12-29 02:13:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/ViolationRecordRepository.java
+++ src/main/java/com/example/demo/repository/ViolationRecordRepository.java
@@ -8,3 +8,18 @@ public interface ViolationRecordRepository extends JpaRepository<ViolationRecord
     List<ViolationRecord> findByUserId(Long userId);
     List<ViolationRecord> findByResolvedFalse();
 }
+
+
+
+
+
+
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.*;
+// import java.util.*;
+// public interface ViolationRecordRepository {
+//     ViolationRecord save(ViolationRecord v);
+//     Optional<ViolationRecord> findById(Long id);
+//     List<ViolationRecord> findByResolvedFalse();
+// }
