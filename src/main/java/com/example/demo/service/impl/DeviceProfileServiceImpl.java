fbb540c3eab2975b467c6f65b64aedef3a149b0e
bala-amypo <bala@amypo.in>
2025-12-29 05:22:53 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
@@ -1,38 +1,38 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import com.example.demo.entity.DeviceProfile;
-// import com.example.demo.repository.DeviceProfileRepository;
-// import com.example.demo.service.DeviceProfileService;
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.stereotype.Service;
+import com.example.demo.entity.DeviceProfile;
+import com.example.demo.repository.DeviceProfileRepository;
+import com.example.demo.service.DeviceProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// @Service
-// @RequiredArgsConstructor
-// public class DeviceProfileServiceImpl implements DeviceProfileService {
+@Service
+@RequiredArgsConstructor
+public class DeviceProfileServiceImpl implements DeviceProfileService {
 
-//     private final DeviceProfileRepository deviceRepo;
+    private final DeviceProfileRepository deviceRepo;
 
-//     @Override
-//     public DeviceProfile registerDevice(DeviceProfile device) {
-//         return deviceRepo.save(device);
-//     }
+    @Override
+    public DeviceProfile registerDevice(DeviceProfile device) {
+        return deviceRepo.save(device);
+    }
 
-//     @Override
-//     public Optional<DeviceProfile> findByDeviceId(String deviceId) {
-//         return deviceRepo.findByDeviceId(deviceId);
-//     }
+    @Override
+    public Optional<DeviceProfile> findByDeviceId(String deviceId) {
+        return deviceRepo.findByDeviceId(deviceId);
+    }
 
-//     @Override
-//     public DeviceProfile updateTrustStatus(Long id, boolean isTrusted) {
-//         DeviceProfile device = deviceRepo.findById(id).orElse(null);
-//         if(device == null) return null;
+    @Override
+    public DeviceProfile updateTrustStatus(Long id, boolean isTrusted) {
+        DeviceProfile device = deviceRepo.findById(id).orElse(null);
+        if(device == null) return null;
 
-//         device.setIsTrusted(isTrusted);
-//         return deviceRepo.save(device);
-//     }
-// }
+        device.setIsTrusted(isTrusted);
+        return deviceRepo.save(device);
+    }
+}
 
 
 
